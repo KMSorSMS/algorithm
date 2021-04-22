0b944dc5e8b20f4adaafa7e9168b9e06b19590d0
yuanzw <yuanzw@shanghaitech.edu.cn>
2021-04-22 18:07:19 +0800

    final change for struct_solution

--- inn_struct.c
+++ inn_struct.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+ #include<stdio.h>
 
 struct queue
 {
@@ -7,24 +7,32 @@ struct queue
     int tail;
 };
 
-int main(){
-    printf("Start!");
+ int main(){
+printf("Start!\n");
 struct queue q;
 q.head = 1;
 q.tail = 10;
 int n;
+
 printf("Please input your target size:\n");
-scanf("%d",n);
-printf("Now set your encoded number:");
+scanf("%d",&n);
+printf("Now set your encoded number:\n");
 for(int i = 1;i<=n;i++){
-scanf("%d",q.data[i]);
+scanf("%d",&q.data[i]);
+printf("%d \n",q.data[i]);
 }
+printf("-----------\n");
+
+for(int i = 1;i<=n;i++){
+printf("%d ",q.data[i]);
+}
+printf("-----------\n");
 while(q.head<q.tail){
-    printf("%d",&q.data[q.head]);
+    printf("%d ",q.data[q.head]);
     q.head++;
     q.data[q.tail] = q.data[q.head];
     q.tail++;
-    q.head++; 
+    q.head++;  
 }
-return 0;
-}
+ return 0;
+ }
